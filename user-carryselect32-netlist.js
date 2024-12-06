b430749cdbe9ef379a5c596d1cfcde6122e45614
KaneLee22 <lzkai@bu.edu>
2024-12-06 20:41:56 +1100

    a

--- user-carryselect32-netlist.js
+++ user-carryselect32-netlist.js
@@ -3,7 +3,7 @@
     "type": "mux2",
     "connections": {
       "y": "sum_0",
-      "s": "sel",
+      "s": "sel_0",  // 確保 `sel_0` 是來自有效邏輯源
       "d0": "S1",
       "d1": "S2"
     },
@@ -21,7 +21,7 @@
     "type": "mux2",
     "connections": {
       "y": "sum_1",
-      "s": "sel",
+      "s": "sel_1",  // 確保 `sel_1` 是來自有效邏輯源
       "d0": "S1",
       "d1": "S2"
     },
@@ -39,7 +39,7 @@
     "type": "mux2",
     "connections": {
       "y": "sum_2",
-      "s": "sel",
+      "s": "sel_2",  // 確保 `sel_2` 是來自有效邏輯源
       "d0": "S1",
       "d1": "S2"
     },
@@ -57,7 +57,7 @@
     "type": "mux2",
     "connections": {
       "y": "sum_3",
-      "s": "sel",
+      "s": "sel_3",  // 確保 `sel_3` 是來自有效邏輯源
       "d0": "S1",
       "d1": "S2"
     },
@@ -78,143 +78,10 @@
       "b": "B",
       "cin": "Cin",
       "s": "S",
-      "cout": "Cout"
+      "cout": "Cout1_0"  // 確保進位連接到正確的進位信號
     },
     "properties": {
       "name": "FA1_0",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "fa",
-    "connections": {
-      "a": "A",
-      "b": "B",
-      "cin": "Cin",
-      "s": "S",
-      "cout": "Cout"
-    },
-    "properties": {
-      "name": "FA1_1",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "fa",
-    "connections": {
-      "a": "A",
-      "b": "B",
-      "cin": "Cin",
-      "s": "S",
-      "cout": "Cout"
-    },
-    "properties": {
-      "name": "FA2_0",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "fa",
-    "connections": {
-      "a": "A",
-      "b": "B",
-      "cin": "Cin",
-      "s": "S",
-      "cout": "Cout"
-    },
-    "properties": {
-      "name": "FA2_1",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "fa",
-    "connections": {
-      "a": "A",
-      "b": "B",
-      "cin": "Cin",
-      "s": "S",
-      "cout": "Cout"
-    },
-    "properties": {
-      "name": "FA3_0",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "fa",
-    "connections": {
-      "a": "A",
-      "b": "B",
-      "cin": "Cin",
-      "s": "S",
-      "cout": "Cout"
-    },
-    "properties": {
-      "name": "FA3_1",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "fa",
-    "connections": {
-      "a": "A",
-      "b": "B",
-      "cin": "Cin",
-      "s": "S",
-      "cout": "Cout"
-    },
-    "properties": {
-      "name": "FA4_0",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "fa",
-    "connections": {
-      "a": "A",
-      "b": "B",
-      "cin": "Cin",
-      "s": "S",
-      "cout": "Cout"
-    },
-    "properties": {
-      "name": "FA4_1",
       "tcd": 2e-11,
       "tpd": 1.2e-10,
       "tr": 4500,
@@ -228,7 +95,7 @@
     "connections": {
       "a": "Cout1_0",
       "b": "Cout1_1",
-      "y": "Cout"
+      "y": "sel_0"  // 使用進位邏輯生成的信號
     },
     "properties": {
       "operation": "MUX",
@@ -246,47 +113,11 @@
     "connections": {
       "a": "Cout2_0",
       "b": "Cout2_1",
-      "y": "Cout"
+      "y": "sel_1"  // 使用進位邏輯生成的信號
     },
     "properties": {
       "operation": "MUX",
       "name": "carry_logic2",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "logic",
-    "connections": {
-      "a": "Cout3_0",
-      "b": "Cout3_1",
-      "y": "Cout"
-    },
-    "properties": {
-      "operation": "MUX",
-      "name": "carry_logic3",
-      "tcd": 2e-11,
-      "tpd": 1.2e-10,
-      "tr": 4500,
-      "tf": 2500,
-      "cin": 5e-15,
-      "size": 27
-    }
-  },
-  {
-    "type": "logic",
-    "connections": {
-      "a": "Cout4_0",
-      "b": "Cout4_1",
-      "y": "Cout"
-    },
-    "properties": {
-      "operation": "MUX",
-      "name": "carry_logic4",
       "tcd": 2e-11,
       "tpd": 1.2e-10,
       "tr": 4500,
